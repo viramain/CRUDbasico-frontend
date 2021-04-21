c4e6cb60945b423cd8d3a6835c7c004ab3b41207
Valentina Iramain <valentinairamain2020@gmail.com>
2021-04-21 10:18:48 -0300

    Se agrego maquetado de agregar producto y sus states

--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,45 +1,51 @@
-import React from "react";
-import { Container,Form,Button } from "react-bootstrap";
+import {React, useState} from "react";
+import { Form,Button, Container, FormCheck, Alert } from "react-bootstrap";
 
 const AgregarProducto = () => {
-    return (
-    <section className='container w-75 px-5'>
-        <h1 className="display-5 text-center py-3">Agregar Nuevo Producto</h1>
+    // creo states por cada opcion del formulario
+    const[nombreProducto,setNombreProducto] = useState('');
+    const[precioProducto,setPrecioProducto] = useState(0);
+    const[categoriaProducto,setCategoriaProducto] = useState('');
 
+    const cambiarCategoria=(e)=>{
+        setCategoriaProducto(e.target.value);
+    }
+    return (
+    <Container>
+    {/* <section className='container w-75 px-5'> */}
         <Form>
+            <h1 className="display-5 text-center py-3">Agregar Nuevo Producto</h1>
             <Form.Group controlId="formNombreProducto" className='py-2'>
                 <Form.Label>Nombre del Producto *</Form.Label>
-                <Form.Control type="text" placeholder="Nombre del producto" />
+                <Form.Control type="text" placeholder="Nombre del producto" onChange={(e)=> {setNombreProducto(e.target.value)}}/>
             </Form.Group>
 
             <Form.Group controlId="formPrecioProducto" className='py-2'>
                 <Form.Label>Precio *</Form.Label>
-                <Form.Control type="number" placeholder="Precio" />
+                <Form.Control type="number" placeholder="0" onChange={(e)=> {setPrecioProducto(parseFloat(e.target.value))}} />
             </Form.Group>
             
-            <Form.Group controlId="formCategoriaProducto" className='py-3 text-center'>
-                <Form.Label className='lead'>Categoría *</Form.Label>
-                {['radio'].map((type) => (
-                <div key={`inline-${type}`} className="mb-3">
-                    <Form.Check inline label="Bebida Caliente" name="group1" type={type} id={`inline-${type}-1`} />
-                    <Form.Check inline label="Bebida Fría" name="group1" type={type} id={`inline-${type}-2`} />
-                    <Form.Check inline label="Sandwich" name="group1" type={type} id={`inline-${type}-3`} />
-                    <Form.Check inline label="Dulce" name="group1" type={type} id={`inline-${type}-4`} />
-                    <Form.Check inline label="Salado" name="group1" type={type} id={`inline-${type}-5`} />
+           {/* form-grup es un div */}
+                <h3 className="text-center py-3">Categoría</h3>
+                <div className='my-3 text-center'>
+                    {/* asignar el valor del value segun como lo quiere la API */}
+                    <FormCheck name='categoria' type='radio' inline label="Bebida Caliente" value='bebidaCaliente' onChange={cambiarCategoria}></FormCheck>
+                    <FormCheck name='categoria' type='radio' inline label="Bebida Fría" value='bebidaFria' onChange={cambiarCategoria}></FormCheck>
+                    <FormCheck name='categoria' type='radio' inline label="Sandwich" value='sandwich' onChange={cambiarCategoria}></FormCheck>
+                    <FormCheck name='categoria' type='radio' inline label="Dulce" value='dulce' onChange={cambiarCategoria}></FormCheck>
+                    <FormCheck name='categoria' type='radio' inline label="Saldado" value='salado' onChange={cambiarCategoria}></FormCheck>
                 </div>
-                ))}
-            </Form.Group>
-            {/* <div className='d-grid gap-2 mb-3'>
-                <Button variant="danger" size="lg" type="submit" block>
-                    Agregar Producto
-                </Button>
-            </div> */}
-            <Button variant="danger" size="lg" block className='mb-3'>
-                Block level button
+            
+            <Button variant="danger" block className='mb-4'>
+                Agregar Producto
             </Button>
+            <Alert variant='warning'>
+                Todos los campos son obligatorios
+            </Alert>
         </Form>
-    </section>   
-  );
+    {/* </section>   */}
+    </Container> 
+    );
 };
 
 export default AgregarProducto;
